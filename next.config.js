5fda7ddb0867227495d6f66d5fa4f35128f98f4e
rgb-000 <bhaktakrpa@gmail.com>
2022-10-23 06:26:05 -0300

    vars up

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/*const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    RPC: '',
+    STORE: '4BDFZHQLNKvtpkqu2ZCCfS9P5DSRu4jAVjG14dBxZG1Q',
+    STORE_BUMP: 255,
+    MINT: 'Aw8kUm8BLR7z42iEpFFfHh3VkwxFXW7ddProfAF5ykNR',
+    TOKEN: 'frPBYare26vksq77U94h7FMGwzAuUeoNqxLE5JCcw8g',
+    CURRENCY: 'PXLSmSBWHU8yAqNof9Ry2LPsZxHSYB4xXsBaQWUmEzV',
+  },
+};*/
+/*const nextConfig = {
   reactStrictMode: true,
   env: {
     RPC: '',
@@ -9,6 +20,20 @@ const nextConfig = {
     TOKEN: '4fZbkEYicCJxzZug5Fx6q27YHQ7EthLKpUqo8ZcjVYNj',
     CURRENCY: 'So11111111111111111111111111111111111111112',
   },
+};*/
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    RPC: '',
+    STORE: '4BDFZHQLNKvtpkqu2ZCCfS9P5DSRu4jAVjG14dBxZG1Q',
+    STORE_BUMP: 255,
+    MINT: 'H5NjdhE7s6duSZ8VezeJCaXWoDv5FWseNtPHzqLAZfrK',
+    TOKEN: '5LwaDXLUv1FDeQANTsyLggSMufEKo9xcbiVXNP3HJ1Y9',
+    CURRENCY: 'So11111111111111111111111111111111111111112',
+  },
 };
 
 module.exports = nextConfig;
+
+
