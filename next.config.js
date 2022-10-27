dab35107a7af9b7b766fd20fa6e4d10393cf6348
rgb-000 <bhaktakrpa@gmail.com>
2022-10-26 22:02:48 -0300

    up!

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     RPC: 'https://ssc-dao.genesysgo.net/',
     STORE: '4BDFZHQLNKvtpkqu2ZCCfS9P5DSRu4jAVjG14dBxZG1Q',
     OWNER: 'rgbxqdf7E3WJEwPHBnuwtDkgQ9AEghMYMC2pYdDxnkt',
-    MINT: 'CWagE6tmLgq9Fujqp6oLBGm2P9WdZ3Anzi436Q7k1dsj',
-    TOKEN: 'EUogvG6QfbFvhgPp9jQ6qyiFds6kxvgchaXb67kqFFu3',
+    MINT: 'EwHmyU51kqckhqcCZUa691sXT3VKDRS31to1jEWUNttM',
+    TOKEN: 'DugNeL3nC2ursSGQu99wVpxQXSBTU3HfmHih2kfnF4V8',
     CURRENCY: 'PXLSmSBWHU8yAqNof9Ry2LPsZxHSYB4xXsBaQWUmEzV',
     START: 1667246400,
     PRICE: 240
