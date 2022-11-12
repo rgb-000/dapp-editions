b77e8597bb33262a8ac16c2730656f6386758bfd
rgb-000 <bhaktakrpa@gmail.com>
2022-11-11 22:03:25 -0300

    paused

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -127,7 +127,7 @@ export const HomeView: FC = ({}) => {
             }}
             className="mint text-center text-1xl px-6 py-3 text-black"
           >
-            {clicked ? 'Cloning...' : clone}
+            MINT PAUSED (WE WILL BACK SOON)
           </button>
         )}
       </div>
